3492379b621266b03019fd6b3a4579861e793c68
Brook25 <bekelebrook24@gmail.com>
2022-04-07 22:13:19 +0000

    edeiting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,12 +9,7 @@
  */
 int main(void)
 {
-	    char str1[70];
-
-	        strcpy(str1, "\"Programming is like building a multilingual puzzle");
-
-		    puts(str1);
-
-		        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
