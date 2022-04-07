52bc9eec7e3b008e00f4be6c09c436eff2efaf21
Brook Endale <bekelebrook24@gmail.com>
2022-04-07 16:21:10 -0700

    creating 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My first, printf task
+ *Return: return 1 after printing the function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
