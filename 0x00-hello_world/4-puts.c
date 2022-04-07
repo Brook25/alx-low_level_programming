3277bed43c7ae7eaad6781bc28cc6a648205fb9a
Brook25 <bekelebrook24@gmail.com>
2022-04-07 22:06:18 +0000

    creating 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	    char str1[70];
+
+	        strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+		    puts(str1);
+
+		        return (0);
+}
+
