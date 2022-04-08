0245be01f3cd21cf27870210468bc7ec8484f886
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 14:37:37 -0700

    new file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+int ch;
+int CH;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (CH = 'A'; CH <= 'Z'; CH++)
+{
+putchar(CH);
+}
+putchar ('\n')
+return (0);
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e')
+if (ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
