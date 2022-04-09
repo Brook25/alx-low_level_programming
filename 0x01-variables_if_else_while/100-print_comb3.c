8293b1a31595a7b08f0a44a6aacbf782fd48ab77
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 21:40:02 -0700

    doo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ for (num2 = num1 + 1; num2 < 10; num2++)
 {
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
-if (num1 == 8 && num == 9)
+if (num1 == 8 && num2 == 9)
 continue;
 putchar(',');
 putchar(' ');
