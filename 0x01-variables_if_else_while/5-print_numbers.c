5bbc360a42c6c27d35a24670659d3ef07950cebf
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 20:08:20 -0700

    added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+printf("%i\n", num);
+}
+return (0);
+}
