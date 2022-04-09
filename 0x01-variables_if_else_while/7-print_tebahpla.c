09359f1d32f7c0f120a61c1046d538a64e176be6
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 20:30:19 -0700

    create

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 122; ch >= 97; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0)
+}
