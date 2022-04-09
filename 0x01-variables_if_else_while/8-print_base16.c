ba1a4ecde6cb84d09d4da896a1c6127a2a9939e7
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 20:43:37 -0700

    addd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alpha;
+for (alpha = 48; alpha <= 57; alpha++)
+{
+putchar(alpha);
+}
+for (alpha = 97; alpha <= 102; alpha++)
+{
+putchar(alpha);
+}
+putchar ('\n');
+return  (0);
+}
