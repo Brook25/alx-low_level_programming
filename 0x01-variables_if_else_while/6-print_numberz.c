b56d17fd07b2a9a0254834808e22b456c62e3c46
Brook Endale <bekelebrook24@gmail.com>
2022-04-08 20:23:39 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+{
+putchar (num);
+}
+putchar ('\n');
+return (0);
+}
