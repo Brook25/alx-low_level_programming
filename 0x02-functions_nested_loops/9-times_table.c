8d6228d763d0fb7bf4e149cce5f2de3bee11999f
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 15:56:30 -0700

    newcomm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,6 +31,16 @@ _putchar(',');
 _putchar (' ');
 }
 }
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+}
 }
 _putchar ('\n');
 }
