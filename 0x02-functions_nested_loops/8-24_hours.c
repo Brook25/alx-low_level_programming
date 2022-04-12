8c57ca1913c9c192a3ccc24c2c7e4e8e336bb562
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 15:30:50 -0700

    tasknew

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of 24 hrs
- * 
+ *
  * Return: Always 0
  */
 void jack_bauer(void)
@@ -15,7 +15,7 @@ for (b = 0; b <= 59; b++)
 {
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
-_putchar (';');
+_putchar (':');
 _putchar (b / 10 + '0');
 _putchar (b % 10 + '0');
 _putchar ('\n');
