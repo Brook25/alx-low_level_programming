11d0d4c873a528becbb2fc5e9e76b0a48e63f7aa
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 16:58:58 -0700

    comcom

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_time_table - print time tables for n
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 int i,j,res;
 
-if (!(n > 15 || n < 0)
+if (!(n > 15 || n < 0))
 {
 for (i = 0; i <= n; i++)
 {
@@ -22,6 +22,7 @@ _putchar(',');
 _putchar(' ');
 }
 if (res < 10 && j != 0)
+{
 _putchar(' ');
 _putchar(' ');
 _putchar((res % 10) + '0');
