a75dd01570cde301c97846af5cf94c3661641e2a
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 16:17:26 -0700

    final

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  *@n: the number where counting starts
  * Return: Always 0
  */
-voint print_to_98(int n)
+void print_to_98(int n)
 {
 if (n < 98)
 {
@@ -16,7 +16,7 @@ printf("%d\n", 98);
 else
 {
 for (n = n; n > 98; n--)
-printf("d, ", n);
+printf("%d, ", n);
 printf("%d\n", 98);
 }
 }
