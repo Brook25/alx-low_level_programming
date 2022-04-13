934e69ca8f3fbd9ba4d8ea8f1fa9c8bb9c75c775
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 18:51:02 -0700

    task1522

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-unsigned long count,i, j, k, sums;
+unsigned long count, i, j, k, sums;
 i = sums = 0;
 j = 1;
 for (count = 0; count < 50; count++)
@@ -22,4 +22,4 @@ sum += k;
 }
 printf("%lu\n", sums);
 return (0);
-
+}
