68d07ff8a066951f13dc279c22efdc11a7970fa5
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 18:49:56 -0700

    task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+/**
+ * main - prints all the multiples of 3 or 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long count,i, j, k, sums;
+i = sums = 0;
+j = 1;
+for (count = 0; count < 50; count++)
+{
+k = i + j;
+i = j;
+j = k;
+if (k % 2 == 0 && k < 4000000)
+{
+sum += k;
+}
+}
+printf("%lu\n", sums);
+return (0);
+
