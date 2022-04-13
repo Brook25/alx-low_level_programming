0d824384852368c918f633004f1a2f0ba02ed2a1
Brook Endale <bekelebrook24@gmail.com>
2022-04-12 17:25:27 -0700

    finres

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - prints all the multiples of 3 or 5
+ *
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+int i, res;
+
+for (i = 0; 1 < 1024; i++
+if (i % 3 == 0 || i % 5 ==0)
+res += i;
+printf("%d\n", res);
+return (0);
+}
