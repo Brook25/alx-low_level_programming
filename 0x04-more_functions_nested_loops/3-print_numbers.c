bac4d84b6dde8f47d3b31f3983cb9318af3d8426
Brook Endale <bekelebrook24@gmail.com>
2022-04-14 10:27:54 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print digits
+ *
+ * Return: 0 Always
+ */
+void print_numbers(void)
+{
+int a;
+i = 0;
+while (i < 10)
+{
+_putchar (a + '0');
+i++;
+}
+_putchar('\n');
+}
