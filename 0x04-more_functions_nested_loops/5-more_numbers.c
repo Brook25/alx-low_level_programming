80b567b6901ee1cdcd4183414f07e498e3cdb514
Brook Endale <bekelebrook24@gmail.com>
2022-04-14 12:03:09 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 to 14 10 times
+ *
+ * Return: Always (0)
+ */
+void more_numbers(void)
+{
+int a;
+int num1;
+int num2;
+int res;
+
+a = 0;
+res = 0;
+while (a < 10)
+{
+while (res <= 14)
+{
+if (res < 10)
+{
+num2 = res;
+}
+else
+{
+num1 = res / 10;
+num2 = res % 10;
+_putchar (num1 + '0')
+}
+_putchar (num2 + '0')
+res++;
+}
+a++;
+result = 0;
+_ptuchar('\n')
+}
+}
