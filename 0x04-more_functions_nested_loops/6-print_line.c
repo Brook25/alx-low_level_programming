801aef47540c98bd62713d9f9a346a6fbec61018
Brook Endale <bekelebrook24@gmail.com>
2022-04-14 12:35:58 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ *@n: print num
+ * Return: 0
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+putchar ('\n')
+}
+else 
+{
+putchar ('\n');
+}
+}
