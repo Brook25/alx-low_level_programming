211beff118a6a180f84956188b700808f61a3568
Brook Endale <bekelebrook24@gmail.com>
2022-04-14 17:33:42 -0700

    fintask

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_number - prints integer
- * @n: integer print
+ * print_number - prints int
+ * @n: takes number
  *
- * Return: Always 0
+ * Return: void
  */
 void print_number(int n)
 {
 unsigned int m;
 if (n < 0)
 {
-_putchar(' ');
+_putchar('-');
 m = -n;
 }
 else
