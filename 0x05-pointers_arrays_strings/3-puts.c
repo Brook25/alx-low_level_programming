f4f4fa40144397184ff62de4eb8072026ff47c60
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 14:37:25 +0300

    taskk4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _puts - prints a string
+ * print_rev - prints a string
  * @str: a pointer to a string
  * Return: Always 0
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
-char *c;
 int i;
-c = str;
-for (i = 0; c[i]; i++)
+int j;
+s[i] = '\0';
+for (j < i; j--)
 {
-_putchar (c[i]);
+_putchar (c[j]);
 }
 _putchar ('\n');
 }
