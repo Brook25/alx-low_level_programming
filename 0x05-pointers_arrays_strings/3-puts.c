cd6f9fc98a1f54c3e3dc0e0274167aa42b132cd6
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 14:03:43 +0300

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: a pointer to a string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+int i = 0;
+for (str[i]++;)
+{
+putchar (str[i])
+}
+putchar ('\n')
+}
