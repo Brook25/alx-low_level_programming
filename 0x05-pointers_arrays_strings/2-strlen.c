8578b3a2bb23ec2bebe1c11926a7bfed904b132c
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 13:08:06 +0300

    1st try

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * _strlen - returns string length
+ * @s: string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int i = *s[0];
+int j = 0;
+for (i = 1; i++)
+j+=1
+return (j)
+}
+
