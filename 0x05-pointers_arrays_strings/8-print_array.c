9635ce2a5ef6fa6245f95c2449681ade5d292144
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 17:25:29 +0300

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integer
+ * @a: A pointed to an int to be changed
+ * @n: return value n
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+if (i < n -1)
+{
+printf(", ");
+}
+i++;
+}
+printf("\n");
+
