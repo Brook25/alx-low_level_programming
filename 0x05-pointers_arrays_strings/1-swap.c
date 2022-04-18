6ecb1aa4f8079d7a1166636d7c3a58fedf9ec410
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 12:42:53 +0300

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps integers
+ * @a: 1st integer to be swapped
+ * @b: 2nd integer to be swappe
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+int t;
+t = a;
+a = b;
+b = t;
+}
