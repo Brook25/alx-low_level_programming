c6fc83ef181f9d7db241c5e1807d603d4b99f278
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 19:30:51 +0300

    taskfinal

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,28 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-int num;
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+char a[100];
+int r, p, q;
+p = 0;
+q = 0;
+srand(time(NULL));
+while (p < 2645)
+{
+r = rand() % 122;
+if (r > 32)
+{
+a[i++] = r
+a == r;
+}
+}
+a[i++] = (2772 - p);
+a[i] = '\0';
+printf("%s", a);
 return (0);
 }
