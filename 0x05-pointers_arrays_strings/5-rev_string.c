29af0be28b89d690b6d648c6c3f9fe6ecdeed245
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 17:01:54 +0300

    newedittt

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,11 +16,11 @@ length++;
 count = length;
 start_c = s;
 end_c = s;
-for (i = 0; i < count -1; i++)
+for (i = 0; i < count - 1; i++)
 {
 end_c++;
 }
-for(i = 0; i < count / 2; i++)
+for (i = 0; i < count / 2; i++)
 {
 c = *end_c;
 *end_c = *start_c;
