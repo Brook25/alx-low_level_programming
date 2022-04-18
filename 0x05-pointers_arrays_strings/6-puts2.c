2f123e7082cac23c72358b80f1dcf6252517e9ee
Brook Endale <bekelebrook24@gmail.com>
2022-04-18 17:06:40 +0300

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every character in a string
+ * @str: a pointer to an int
+ * Return: Always 0
+ */
+void puts(char *str)
+{
+int a;
+for (a = 0; str[a] != '/0'; a++)
+{
+if (a % 2 == 0
+{
+_putchar (str[a]);
+}
+_putchar ('\n');
+}
