cdcc8ce83a7b1a4cf4193014cb88391f4393793c
Brook Endale <bekelebrook24@gmail.com>
2022-04-21 11:06:53 +0300

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 
 char *leet(char *s)
 {
-int count = 0, 
+int count = 0, i;
 int lower_case[] = {97, 101, 111, 116, 108};
 int upper_case[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
