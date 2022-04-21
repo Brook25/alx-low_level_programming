4d1ac9683034cfa9ff183591a6b93fed4d654129
Brook Endale <bekelebrook24@gmail.com>
2022-04-21 18:19:06 +0300

    task1003

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * infinite_add - adds two numbers
  * @n1: First number.
@@ -10,36 +9,36 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int add = 0, i, j, ln1, ln2;
-for (ln1 = 0; n1[ln1]; ln1++)
-;
-for (ln2 = 0; n2[ln2]; ln2++)
-;
-if (ln1 > size_r || ln2 > size_r)
-return (0);
-ln1--;
-ln2--;
-size_r--;
-for (i = 0; i < size_r; i++, ln1--, ln2--)
-{
-if (ln1 >= 0)
-add += n1[ln1] - '0';
-if (ln2 >= 0)
-add += n2[ln2] - '0';
-if (ln1 < 0 && ln2 < 0 && add == 0)
-break;
-r[i] = add % 10 + '0';
-add /= 10;
-}
-r[i] = '\0';
-if (ln1 >= 0 || ln2 >= 0 || add) 
-return (0);
-for (i--, j = 0; i > j; i--, j++)
-{
-add = r[i];
-r[i] = r[j];
-r[j] = add;
-}
-return (r);
-}
+	int add = 0, len1, len2, i, j;
 
+	for (len1 = 0; n1[len1]; len1++)
+	;
+	for (len2 = 0; n2[len2]; len2++)
+	;
+	if (len1 > size_r || len2 > size_r)
+		return (0);
+	len1--;
+	len2--;
+	size_r--;
+	for (i = 0; i < size_r; i++, len1--, len2--)
+	{
+		if (len1 >= 0)
+			add += n1[len1] - '0';
+		if (len2 >= 0)
+			add += n2[len2] - '0';
+		if (len1 < 0 && len2 < 0 && add == 0)
+			break;
+		r[i] = add % 10 + '0';
+		add /= 10;
+	}
+	r[i] = '\0';
+	if (len1 >= 0 || len2 >= 0 || add)
+		return (0);
+	for (i--, j = 0; i > j; i--, j++)
+	{
+		add = r[i];
+		r[i] = r[j];
+		r[j] = add;
+	}
+	return (r);
+}
