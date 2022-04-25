b0e4378e2f201f77686e096eb594443c95f61ab7
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 20:05:14 +0300

    task10

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets value of pointer to char
+ * @s: value to be set
+ * @to: string
+ * Retutn: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
