3cde617bcbb6848ddec2a2d871e4f1cc6ca19857
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 19:37:35 +0300

    task9

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - does the sum of two diagonals of a square matrix
- * @a: array of characs
+ * print_diagsums - sum of two diagonals in a square matrix
+ * @a; array of characs
  * @size: matrix
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
 int b;
-int tot1, tot2;
-tot1 = 0;
-tot2 = 0;
+int sum1, sum2;
+sum1 = 0;
+sum2 = 0;
 b = 0;
 while (b < size)
 {
-tot1 = tot1 + *(a + b * size + b);
-tot2 = tot2 + *(a + b * size + size - b - 1);
+sum1 = sum1 + *(a + b * size + b);
+sum2 = sum2 + *(a + b * size + size - b - 1);
 b++;
 }
-printf("%i, %i\n", tot1, tot2);
+printf("%i, %i\n", sum1, sum2);
 }
