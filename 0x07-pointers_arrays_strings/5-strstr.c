e4f8dadfc66d3188bc78625ce8f5ae169fcd9ee1
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 18:16:04 +0300

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - first occurence of substring
+ * @haystack: main str to be checked
+ * @needle: substring in the haystack
+ * Return: 0
+ */
+char  *_strstr(char *haystack, char *needle)
+{
+char *s, char *i;
+while (*haystack != '\0')
+{
+s = haystack;
+i = needle;
+while (*haystack != '\0' && *i != '\0' && *haystack == *i)
+{
+haystack++;
+i++;
+}
+if (i == '\0')
+return (s);
+haystack = s + 1;
+}
+return (0);
+}
