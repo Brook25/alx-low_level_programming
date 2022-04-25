292f95b9dc37276d7c7130146db451059a86e186
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 13:30:45 +0300

    task00

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,12 +6,12 @@
  * @n: first n bytes
  * Return: pointer
  */
-char *_memset(char *s, char b,unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
 {
 *(s + i) = b;
 }
-return (s + i);
+return (s);
 }
