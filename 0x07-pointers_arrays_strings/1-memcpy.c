5431547a24e66de505576e19f82bf8d6316e2a48
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 13:38:53 +0300

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,14 +4,14 @@
  * @dest: dest buffer to copy the memory area to
  * @src: source where copy is done from
  * @n: bytes of memory
- * Return: src
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-*(dest+i) += *(src+i);
+*(dest + i) += *(src + i);
 }
 return (dest);
 }
