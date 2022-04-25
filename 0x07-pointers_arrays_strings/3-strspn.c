30d5882f35a46f5388eccb888923ea4d2d7e0871
Brook Endale <bekelebrook24@gmail.com>
2022-04-25 17:44:02 +0300

    task33

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,21 +3,20 @@
  * _strspn - outputs the number of bytes of a substirng
  * @s: The string
  * @accept: substirng
- * Return: return total
+ * Return: total
  */
-unisgned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 int a, b;
 int count = 0;
-st1 = s;
-st2 = accept;
+
 a = 0;
-while (st1[a] != '\0')
+while (s[a] != '\0')
 {
 b = 0;
-while (st2[b] != '\0')
+while (accept[b] != '\0')
 {
-if (st2[b] == st1[a])
+if (accept[b] == s[a])
 {
 count++;
 break;
