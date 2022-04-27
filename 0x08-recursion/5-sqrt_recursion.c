759da684596531aee97245c4bf06dad819c9fe95
Brook Endale <bekelebrook24@gmail.com>
2022-04-27 17:47:13 +0300

    task55

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function returns square root of a number
+ * @n: number input
+ * Return: int
+ */
+int _sqrt_recurssion(int n)
+{
+if (n == 0)
+return (0);
+if (n == 1)
+return (1);
+if (n < 0)
+return (-1);
+return (sqrnum(n, 1));
+}
+/**
+ * sqrnum - function does the sqr root of a number
+ * @num: number input
+ * @i; variable
+ * Return: int
+ */
+int sqrnum(int num, int i)
+{
+if (num == (i * i))
+return (i);
+else if (num > (i * i))
+return (sqrnum(num, i + 1));
+else
+return (-1
+}
