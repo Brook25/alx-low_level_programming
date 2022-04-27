d097abed475843947c6bb256c6e240789fac9c28
Brook Endale <bekelebrook24@gmail.com>
2022-04-27 21:18:09 +0300

    finaltaks

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,9 +13,9 @@ if (*s2 != '\0' && *s2 == '*')
 {
 return (wildcmp(s1, s2 + 1));
 }
-if (*s2 == '\0')
+return (*s2 == '\0');
 }
-if (*s2 == '\0')
+if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
