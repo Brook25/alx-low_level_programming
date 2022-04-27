e6c7586726f5d9227ac353a9dd75791aafc9088a
Brook Endale <bekelebrook24@gmail.com>
2022-04-27 20:18:03 +0300

    task666

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,8 +4,6 @@
  * is_prime_number - returns 1 if the input integer is a prime number
  * otherwise return 0
  * @n: input number
- * @num: number input
- * @i: variable to count
  * Return: int
  */
 int is_prime_number(int n)
@@ -17,6 +15,8 @@ int is_prime_number(int n)
 
 /**
  * PrimNum - function that gets if num is 0 or 1
+ * @num: number input
+ * @i: variable to count
  * Return: int
  */
 
