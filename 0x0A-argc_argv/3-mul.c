75ba5854a74245f4df371d72b61f23c90c5f1120
Brook Endale <bekelebrook24@gmail.com>
2022-04-29 22:06:48 +0300

    task33

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,29 +8,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int count, num1, num2, res;
+int count, num1, num2, res;
 
-	count = num1 = num2 = res = 0;
-	if (argc > 0)
-	{
-		while (count < argc)
-		{
-			if (argc == 3)
-			{
-				if (count == 1)
-					num1 = atoi(argv[count]);
-				else if (count == 2)
-					num2 = atoi(argv[count]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			res = (num1 * num2);
-			count++;
-		}
-		printf("%d\n", res);
-	}
-	return (0);
+count = num1 = num2 = res = 0;
+if (argc > 0)
+{
+while (count < argc)
+{
+if (argc == 3)
+{
+if (count == 1)
+num1 = atoi(argv[count]);
+else if (count == 2)
+num2 = atoi(argv[count]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+res = (num1 * num2);
+count++;
+}
+printf("%d\n", res);
+}
+return (0);
 }
