a63899e3cac52c5b6282906d78ab05500c747d37
Brook Endale <bekelebrook24@gmail.com>
2022-04-29 22:21:52 +0300

    task444

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * check_digit - checks if a given char is number or not
+ * @a: input char
+ * Return: int
+ **/
+
+int check_digit(char *a)
+{
+	int b, num, len;
+
+	b = 0;
+	num = 0;
+	len = strlen(a);
+	while (b < len)
+	{
+		if (a[b] < '0' || a[b] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (a[b] - '0');
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * main -  program that adds positive numbers
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: int
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i, num, res;
+
+	res = 0;
+	for (i = 1; i < argc; i++)
+	{
+		num = check_digit(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res += num;
+	}
+	printf("%d\n", res);
+	return (0);
+}
