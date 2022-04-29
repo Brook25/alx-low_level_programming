5f110ab4e86b02722a8378b9e1db84d8206ed8f4
Brook Endale <bekelebrook24@gmail.com>
2022-04-29 21:56:43 +0300

    newwtaks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,22 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - program that prints its own name
- * @argc: argument vector
- * argv: argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: int
  */
-
 int main(int argc, char *argv[])
 {
-	int count;
-	for (count = 0; count < argc; count++)
-	{
-		if (argc == 1)
-	printf("%s\n", argv[count]);
-	else
-		break;
-	}	
+int count;
+for (count = 0; count < argc; count++)
+{
+if (argc == 1)
+printf("%s\n", argv[count]);
+else
+break;
+}
 return (0);
 }
