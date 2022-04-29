f3ecf8fe9993b1ab6cfc47ac24b8fc28c13cd767
Brook Endale <bekelebrook24@gmail.com>
2022-04-29 22:17:04 +0300

    newtassk

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all arguments it is given
+ * main - program that prints all arguments it receives
  * @argc: argument count
  * @argv: argunment vector
  *
@@ -11,29 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-int count, num1, num2, res;
+	int count, n, num2, res;
 
-count = num1 = num2 = res = 0;
-if (argc > 0)
-{
-while (count < argc)
-{
-if (argc == 3)
-{
-if (count == 1)
-num1 = atoi(argv[count]);
-else if (count == 2)
-num2 = atoi(argv[count]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-res = (num1 * num2);
-count++;
-}
-printf("%d\n", res);
-}
-return (0);
+	count = n = num2 = res = 0;
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			if (argc == 3)
+			{
+				if (count == 1)
+					n = atoi(argv[count]);
+				else if (count == 2)
+					num2 = atoi(argv[count]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			res = (n * num2);
+			count++;
+		}
+		printf("%d\n", res);
+	}
+	return (0);
 }
