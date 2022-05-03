8c9f93af071bb39fd8332a57140cd537bf3631cd
Brook Endale <bekelebrook24@gmail.com>
2022-05-03 10:58:49 +0300

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+/**
+ * str_concat - returns pointer to a memory that contains the contents of a
+ * string followed by the contents of another string
+ * @s1: pointer to a string element
+ * @s2: pointer to a string element
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+int i, j, k, l;
+char *nul = "";
+char *ch;
+i = j = k = l = 0;
+if (s1 == NULL)
+s1 = nul;
+if (s2 == NULL)
+s2 = nul;
+while (s1[i]!='\0')
+i++;
+while (s2[j]!='\0')
+j++;
+j += 1;
+ch = malloc(sizeof(char) * (i + j));
+if (ch == NULL)
+return (NULL);
+while (sl[k] != '\0')
+{
+ch[k] = s1[k];
+x += 1;
+}
+
+while (s2[l] != '\0')
+{
+ch[k] = s2[l];
+l += 1;
+k += 1;
+}
+ch[k] = '\0';
+return (ch);
+}
