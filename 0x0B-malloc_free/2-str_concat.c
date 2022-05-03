e2ca39a2c2e2915c42d63f0750883db76b01e320
Brook Endale <bekelebrook24@gmail.com>
2022-05-03 11:03:47 +0300

    taskkk

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ if (s1 == NULL)
 s1 = nul;
 if (s2 == NULL)
 s2 = nul;
-while (s1[i]!='\0')
+while (s1[i] !='\0')
 i++;
-while (s2[j]!='\0')
+while (s2[j] !='\0')
 j++;
 j += 1;
 ch = malloc(sizeof(char) * (i + j));
