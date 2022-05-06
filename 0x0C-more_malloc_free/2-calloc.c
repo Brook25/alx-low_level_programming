16c06ccc8daa0ddde36b4e48d0a8a5bb0bae88e7
Brook Endale <bekelebrook24@gmail.com>
2022-05-06 12:33:34 +0300

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _calloc - alloctes memory for an array
+ * @nmemb: nth member
+ * @size: bytes
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *s;
+unsigned int k;
+if (nmemb == 0 || size == 0)
+return (NULL);
+s = malloc(nmemb * size);
+if (s == NULL)
+return (NULL);
+for (k = 0; k < nmemb * size; k++)
+s[k] = 0;
+return (s);
+}
