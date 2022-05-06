5d1f1950ba412ccb90a89158185438c5be16c31e
Brook Endale <bekelebrook24@gmail.com>
2022-05-06 22:21:17 +0300

    taskfinal

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,7 +6,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 #endif
