ee674555554620e8b2192794113a3946248e5936
Brook Endale <bekelebrook24@gmail.com>
2022-05-06 16:52:54 +0300

    taskfin

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,25 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mun;
-int a, b;
-if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
-for (a = 1; a < argc; a++)
-{
-for (b = 0; argv[a][b] != '\0'; b++)
-{
-if (argv[a][b] > 57 || argv[a][b] < 48)
-{
-printf("Error\n");
-exit(98);
-}
-}
-}
-mun = atol(argv[1]) *atol(argv[2]);
-printf("%lu\n", mun);
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 return (0);
 }
