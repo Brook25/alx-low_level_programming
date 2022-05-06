9acd2b4f50acf42467ed26817fd52f8c7773bf2e
Brook Endale <bekelebrook24@gmail.com>
2022-05-06 12:55:02 +0300

    task33

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * array_range - creates an array of int
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+int num, k;
+if (min > max)
+return (0);
+num = malloc((max - min + 1) * sizeof(int));
+if (num == NULL)
+return (0);
+while (k <= max - min)
+num[k++] = min++;
+return (num);
+}
