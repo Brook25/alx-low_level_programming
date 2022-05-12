1fbfe0588aabc08455a806a105b9f5e4d2a03012
Brook Endale <bekelebrook24@gmail.com>
2022-05-12 16:14:22 +0300

    First task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * sum_them_all - function returns sum of all of its parameters
+ * @n: number of parameters
+ * Return: sum of all parameters
+ */
+int sum_them_all(const unsigned int n,...)
+{
+va_list num;
+int sum;
+unsigned int a;
+if (n == 0)
+return (0);
+sum = 0;
+va_start(numbers, n);
+for (a = 0; a < n; a++)
+sum += va_arg(num, int);
+va_end(num);
+return(sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+Variadic funcitons
+Variadic funcitons take variable number of arguments
+stdarg.h
+va_list
+va_start
+va_arg
+va_end
