684ecc0988bd2af98ae1700bfc9d8277af12fabf
Brook Endale <bekelebrook24@gmail.com>
2022-05-19 14:59:31 +0300

    creating a function that adds a new node at the end of a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@
 
 void free_list(list_t *head)
 {
-    list_t *curr;
+	list_t *curr;
 
-    while ((curr = head) != NULL)
-    {
-        head = head->next;
-        free(curr->str);
-        free(curr);
-    }
+	while ((curr = head) != NULL)
+	{
+	head = head->next;
+	free(curr->str);
+	free(curr);
+	}
 }
