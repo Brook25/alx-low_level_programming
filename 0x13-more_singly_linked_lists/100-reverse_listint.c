041f8d8722685a61eebbd2e8025867f3de7bb527
Brook Endale <bekelebrook24@gmail.com>
2022-05-23 17:13:06 +0300

    Final two tasks

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a linked list
+ * @head: head of list
+ *
+ * Return: pointer to first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *a;
+listint_t *z;
+a = NULL;
+z = NULL;
+while (*head != NULL)
+{
+z = (*head)->next;
+(*head)->next = a;
+a = *head
+*head = z;
+}
+*head = z;
+return (*head);
+}
