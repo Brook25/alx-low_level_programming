c09e7e64e8a93f4b542fc5ebdc2424135cf4b5d6
Brook Endale <bekelebrook24@gmail.com>
2022-05-23 18:56:54 +0300

    tenth deletes node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		for (a = 0; a < index - 1 && prev != NULL; i++)
+		for (a = 0; a < index - 1 && prev != NULL; a++)
 		{
 			previous = previous->next;
 		}
