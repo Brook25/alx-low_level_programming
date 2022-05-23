5662882ba90dec059870436ff2f92da07fc769c6
Brook Endale <bekelebrook24@gmail.com>
2022-05-23 18:24:27 +0300

    third task adds a new node at the end of a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *nnew;
+listint_t *nnew
 listint_t *tmp;
 (void)tmp;
 nnew = malloc(sizeof(listint_t));
@@ -27,7 +27,7 @@ while (tmp->next != NULL)
 {
 tmp = tmp->next;
 }
-tmp->next = nnew
+tmp->next = nnew;
 }
 return (*head);
 }
