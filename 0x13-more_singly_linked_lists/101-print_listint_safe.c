9c3b52c9f2cfbca599364594064f0e91c6899430
Brook Endale <bekelebrook24@gmail.com>
2022-05-24 16:05:12 +0300

    Twelvth task prints a linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+/**
+ * free_listp - function frees a linked list
+ * @head: head of list
+ *
+ * Return: nothing
+ */
+void free_listp(listp_t **head)
+{
+listp_t *tempo;
+listp_t *curent;
+if (head != NULL)
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
+}
+*head = NULL;
+}
+}
+/**
+ * print_listint_safe - prints linked list
+ * @head: head of list
+ *
+ * Return: num of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
+hptr = NULL;
+while (head != NULL)
+{
+new = malloc(sizeof(listp_t));
+if (new == NULL)
+exit(98);
+new->p = (void *)head;
+new->next = hptr;
+hptr = new;
+add = hptr;
+while (add->next != NULL)
+{
+add = add->next;
+if (head == add->p)
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+free_listp(&hptr);
+return (nnodes);
+}
+}
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+nnodes++
+}
+free_listp(&hptr);
+return (nnodes);
+}
