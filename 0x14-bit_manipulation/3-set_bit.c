8aaae4e2147c1f6220106be5441cd20e57356bc7
Brook Endale <bekelebrook24@gmail.com>
2022-05-26 19:35:32 +0300

    Function sets the value of a bit to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m
+	unsigned int a
 
 	if (index > 63)
 		return (-1);
 
-	m = 1 << index;
-	*n = (*n | m);
+	a = 1 << index;
+	*n = (*n | a);
 
 	return (1);
 }
