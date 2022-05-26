56a03c67c4f0d6342dd4a38c98c324f55e25a2a7
Brook Endale <bekelebrook24@gmail.com>
2022-05-26 19:27:57 +0300

    Function converts binary to unsigned int and other files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _pow_recursion - function returns the value of x
- * raised to the power of y
- * @a: base number
- * @y: pow number
- * Return: int
+ * print_binary - prints binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
-
-int _pow_recursion(int a, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (a * _pow_recursion(a, y - 1));
-}
-
-/**
- * print_binary - function that prints the binary representation of a number
- * @n: decimal number
- * Return: nothing
- */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
-
-	if (n == 0)
-		_putchar('0');
-	while (exp >= 0)
+	if (n >> 0)
 	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
-		{
-			_putchar('1');
-			n -= res_pow;
-			flag = 1;
-		}
-		else if (n < res_pow && flag == 1)
-			_putchar('0');
-		exp--;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
