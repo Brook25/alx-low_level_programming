aeafc1753105f45e35fcf7e848217be889f2b01a
Brook Endale <bekelebrook24@gmail.com>
2022-05-29 18:24:10 +0300

    strlen recursion function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,13 +5,13 @@
  * Return: length
  */
 
-int i;
+int i=0;
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-return(0);
+return (0);
 s++;
 i++;
 _strlen_recursion(s);
-return(i);
+return (i);
 }
