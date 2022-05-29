1ea63a08fae44e0c44e5007242d13de5da6d917c
Brook Endale <bekelebrook24@gmail.com>
2022-05-29 18:17:29 +0300

    strlen recursion function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _strlen_recursion - function returns length of a string
- * @s: string
- * Return: int
+ * _strlen_recursion - function returns length of string
+ * @s: stirng
+ * Return: length
  */
+
+int i;
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-return (0);
+return;
 s++;
-return (1 + _strlen_recursion(s));
+i++;
+_strlen_recursion(s);
+return(i);
 }
