809360069740f422d7d9152493f37be11a6bf3e0
Brook Endale <bekelebrook24@gmail.com>
2022-05-30 17:21:43 +0300

    funciton appends text at the end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (numletters = 0; text_content[numletters]; numletters++)
 			;
 
-		rwr = write(fd, text_content, numletters)
+		rwr = write(fd, text_content, numletters);
 
 		if (rwr == -1)
 			return (-1);
