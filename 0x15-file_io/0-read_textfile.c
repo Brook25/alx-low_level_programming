e997ac3ec664bd520247acd0ef9c430a86aa97d2
Brook Endale <bekelebrook24@gmail.com>
2022-05-30 20:00:27 +0300

    file 0 edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,8 +9,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
-ssize_t nrd, nwr
-char *buff
+ssize_t nrd, nwr;
+char *buff;
 if (!filename)
 return (0);
 fd = open(filename, O_RDONLY);
