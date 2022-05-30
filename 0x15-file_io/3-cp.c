8ef2e5eaec5aaf2c4fc7fc32d2c16e121471e1d1
Brook Endale <bekelebrook24@gmail.com>
2022-05-30 18:29:08 +0300

    funciton checks if file can be opened

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ exit(99);
  */
 int main(int argc, char *argv[])
 {
-int file__from, file__to, err__close;
+int file__from, file__to, err_close;
 ssize_t numchars, nwr;
 char buf[1024];
 if (argc != 3)
@@ -37,7 +37,7 @@ exit(97);
 }
 file__from = open(argv[1], O_RDONLY);
 file__to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-error__file(file__from, file__to, argv);
+error_file(file__from, file__to, argv);
 numchars = 1024;
 while (numchars == 1024)
 {
@@ -46,16 +46,16 @@ if (numchars == -1)
 error_file(-1, 0, argv);
 nwr = write(file__to, buf, numchars);
 if (nwr == -1)
-error__file(0, -1, argv);
+error_file(0, -1, argv);
 }
 err_close = close(file__from);
-if (err__close == -1)
+if (err_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file__from);
 exit(100);
 }
-err__close = close(file__to);
-if (err__close == -1)
+err_close = close(file__to);
+if (err_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file__from);
 exit(100);
