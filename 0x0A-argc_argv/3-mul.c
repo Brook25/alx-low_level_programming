540fc6c1e234c77f1934872fcfba9be1c043df8f
Brook Endale <bekelebrook24@gmail.com>
2022-06-01 19:40:31 +0300

    check edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,39 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - program that prints all arguments it receives
- * @argc: argument count
- * @argv: argunment vector
- *
- * Return: int
- */
-
-int main(int argc, char *argv[])
+*/
+int main (int argc, char *argv[])
 {
-	int count, n, num2, res;
-
-	count = n = num2 = res = 0;
-	if (argc > 0)
-	{
-		while (count < argc)
-		{
-			if (argc == 3)
-			{
-				if (count == 1)
-					n = atoi(argv[count]);
-				else if (count == 2)
-					num2 = atoi(argv[count]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			res = (n * num2);
-			count++;
-		}
-		printf("%d\n", res);
-	}
-	return (0);
+if (argc != 3)
+{
+printf ("Error\n");
+return (1);
+}
+printf ("%d", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }
