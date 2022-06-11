95e3b4ef4824e5d4feb53cad38c35c502f81f191
Brook Endale <bekelebrook24@gmail.com>
2022-06-11 19:15:13 +0300

    Edits

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,11 +50,3 @@ return (NULL);
 }
 return (neww_dog);
 }
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    return (0);
-}
