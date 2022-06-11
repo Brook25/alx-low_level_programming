d5f6547032c586f73d7f8d2eeb72983eae748f15
Brook Endale <bekelebrook24@gmail.com>
2022-06-11 18:39:41 +0300

    edit check

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,63 +1,42 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
-/**
- * _strdup - returns pointer to an allocated space in memory
- * that contains copy of the string parameter.
- * @str: input string
- * Return: character pointer
- */
-
-char *_strdup(char *str)
+char *_strcpy(char *s)
 {
-	char *s;
-	unsigned int a = 0;
-	unsigned int b = 0;
-
-	if (str == NULL)
-		return (NULL);
-	while (str[a] != '\0')
-		a += 1;
-	a += 1;
-	s = malloc(a * sizeof(*s));
-	if (s == NULL)
-		return (NULL);
-	while (b < a)
-	{
-		s[b] = str[b];
-		b += 1;
-	}
-	return (s);
+char *copy;
+int i;
+for(i = 0; s[i] != '\0'; i++)
+;
+copy=malloc(i + 1);
+if (copy == NULL)
+{
+free(copy);
+return (NULL);
 }
-
-/**
- *new_dog - function creates new dog
- *@name: name of dog
- *@age: age of dog
- *@owner: owner
- *Return: struct
- */
+for(i = 0; s[i] != '\0'; i++)
+copy[i] = s[i];
+copy[i] = '\0';
+return (copy);
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+dog_t *neww_dog;
+char *name_copy;
+char *owner_copy;
+name_copy = _strcpy(name);
+owner_copy = _strcpy(owner);
+neww_dog = malloc(sizeof(dog_t));
+if (neww_dog == NULL)
+{
+free (neww_dog);
+return (NULL);
+}
 
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
-		return (NULL);
-	new_dog->name = _strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->age = age;
-	new_dog->owner = _strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog);
-		free(new_dog->name);
-		return (NULL);
-	}
-	return (new_dog);
+neww_dog->name = name_copy;
+neww_dog->age = age;
+neww_dog->owner = owner_copy;
+
+return (neww_dog);
 }
