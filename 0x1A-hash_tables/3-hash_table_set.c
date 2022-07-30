437ce9fd2622a0110042c296390d5086e8edd589
Brook Endale <bekelebrook24@gmail.com>
2022-07-30 15:18:55 +0300

    function adds a value at index of a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,7 +42,7 @@ head = malloc(sizeof(hash_node_t));
 	if (head == NULL)
 	return (0);
 head->key = (char *) key;
-	if (key == NULL || key == "")
+	if (key == NULL || strlen(key) == 0)
 	return (0);
 
 head->value = __strdup(value);
