fd3d046dbe485d45482b5a2ddf66d74a8b1b2837
Brook Endale <bekelebrook24@gmail.com>
2022-07-30 12:01:09 +0300

    Hash table and its implementation advanced task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,25 @@
 #include "hash_tables.h"
-
-
+/**
+ * hash_table_create - function creates a hash table
+ * @size: size of the array of the hash table
+ *
+ * Return: Pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t **hp;
-int i = 0;
-hp = malloc(sizeof(hash_table_t *) * size);
-if (hp == NULL)
-    return NULL;
+unsigned long int i = 0;
+hash_table_t *hp = malloc(sizeof(hash_table_t));
+	if (hp == NULL)
+		return (NULL);
 
+hp->array = malloc(sizeof(hash_node_t *) * size);
+	if (hp->array == NULL)
+    		return NULL;
+
+hp->size = size;
 while (i < size)
 {
-*(hp + i) = NULL;
+*(hp->array + i) = NULL;
 i++;
 }
 return(hp);
