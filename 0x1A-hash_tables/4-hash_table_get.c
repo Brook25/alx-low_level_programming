7495cf000c64230da9c1ac7d0daac89bc6645794
Brook Endale <bekelebrook24@gmail.com>
2022-07-31 18:06:03 +0300

    function gets a value at key in a hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,21 +3,22 @@
  * hash_table_get - gets the value at a certain key in a hashtable
  * @ht: pointer to a hashtable
  * @key: key to search the value with
+ * Return: Value if exists otherwise NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-int index = key_index((const unsigned char *) key, ht->size);
+unsigned long int index = key_index((const unsigned char *) key, ht->size);
 hash_node_t *temp = ht->array[index];
 
-if (ht == NULL || strlen(key) == 0)
+if (ht == NULL || key == NULL)
 	return (NULL);
 
 while (temp != NULL && strcmp(temp->key, key) != 0)
 	temp = temp->next;
 
 if (temp)
-return(temp->value);
+return (temp->value);
 
 return (NULL);
 }
