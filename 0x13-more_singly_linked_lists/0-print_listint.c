45f30d0bb3e59f1291fb7393ab74c6799ce67156
Brook25 <bekelebrook24@gmail.com>
2023-08-14 17:04:36 +0000

    prints a list and returns count

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,23 @@
 #include "lists.h"
 /**
- * print_listint - prints all elements in a list
- * @h: head of list
- *
- * Return: numbers of nodes
+ * print_listint - function prints a list and returns length of list
+ * h: head of the list
+ * Return: length of the list
  */
-size_t print_listint(const listint_t *h)
-{
-size_t nodes = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-nodes++;
-}
-return (nodes);
+
+
+size_t print_listint(const listint_t *h) {
+	listint_t *temp = NULL;
+	int count = 0;
+	if (h) {
+	printf("%d\n", h->n);
+	temp = h->next;
+	count += 1;
+	}
+	while (temp != NULL) {
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count += 1;
+	}
+	return count;
 }
