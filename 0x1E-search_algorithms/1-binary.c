23b6751f8b8b2b1aefb16fa6f41633c9904f5f1f
Brook25 <bekelebrook24@gmail.com>
2023-08-26 12:21:13 +0000

    does binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		printf("Searching in aray: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", i);
